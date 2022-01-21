9aac02c95e9c44d2fb3381a8cce454e2ec1013ab
shubhamsatyam54 <shubhamsatyam54@gmailcom>
2022-01-22 04:15:21 +0530

    22-01-22

---
+++ bill/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Stock(models.Model):
+    pro_id=models.CharField(max_length=50,primary_key=True,blank=False)
+    pro_name=models.CharField(max_length=300)
+    pro_batch=models.CharField(max_length=50)
+    quantity=models.IntegerField()
+    M_date=models.DateField()
+    E_date=models.DateField()
+
+class Party(models.Model):
+    name=models.CharField(max_length=300,primary_key=True)
+    add=models.TextField()
+
+class Rate(models.Model):
+    pro=models.ForeignKey(Stock,on_delete=models.SET_NULL,null=True)
+    party=models.ForeignKey(Party,on_delete=models.SET_NULL,null=True)
+    rt_name=models.CharField(max_length=20,default="default")
+    cost=models.FloatField(blank=False)
