449163c8ff2ebb252eebbe42147f0584c7ae3863
shubhamsatyam54 <shubhamsatyam54@gmailcom>
2022-01-22 06:05:00 +0530

    22-01-22

--- bill/admin.py
+++ bill/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Stock,Rate,Party
+from .models import Stock,Rate,Party,Product,Invoice,Invoice_item
 # Register your models here.
+admin.site.register(Product)
 admin.site.register(Stock)
 admin.site.register(Rate)
 admin.site.register(Party)
+admin.site.register(Invoice)
+admin.site.register(Invoice_item)
+
 
 
--- bill/models.py
+++ bill/models.py
@@ -1,26 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    pro_id = models.CharField(max_length=50, primary_key=True, blank=False)
+    pro_name = models.CharField(max_length=300)
+    avil_quant = models.IntegerField()
+
+
 class Stock(models.Model):
-    pro_id=models.CharField(max_length=50,primary_key=True,blank=False)
-    pro_name=models.CharField(max_length=300)
-    pro_batch=models.CharField(max_length=50)
-    quantity=models.IntegerField()
-    M_date=models.DateField()
-    E_date=models.DateField()
+    pro_id = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    pro_batch = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    M_date = models.DateField()
+    E_date = models.DateField()
+
 
 class Party(models.Model):
-    name=models.CharField(max_length=300,primary_key=True)
-    add=models.TextField()
+    name = models.CharField(max_length=300, primary_key=True)
+    add = models.TextField()
+
 
 class Rate(models.Model):
-    pro=models.ForeignKey(Stock,on_delete=models.SET_NULL,null=True)
-    party=models.ForeignKey(Party,on_delete=models.SET_NULL,null=True)
-    rt_name=models.CharField(max_length=20,default="default")
-    cost=models.FloatField(blank=False)
+    pro = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    party = models.ForeignKey(Party, on_delete=models.SET_NULL, null=True)
+    rt_name = models.CharField(max_length=20, default="default")
+    cost = models.FloatField(blank=False)
+
 
 class Invoice(models.Model):
-    invoice_id=models.CharField(max_length=50,primary_key=True,blank=False)
+    invoice_id = models.CharField(max_length=50, primary_key=True, blank=False)
     total = models.FloatField()
+    gst = models.FloatField()
+    discount = models.FloatField(null=True)
+    round = models.FloatField(null=True)
+    g_total = models.FloatField()
+
+
+class Invoice_item(models.Model):
+    invoice_id = models.ForeignKey(Invoice, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    rate = models.ForeignKey(Rate, to_field='rt_name', on_delete=models.SET_NULL, null=True)
 
 
